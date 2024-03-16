e2c70a36fc3c6e267eb0539f56c913d4078c49bb
Roger Lopez <rllopez0410@gmail.com>
2024-03-16 04:01:31 -0400

    sniffing all prices fron GPUs on ebay

--- src/main.py
+++ src/main.py
@@ -2,5 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p4432023.m570.l1311&_nkw=gpu+graphics+card&_sacat=0"
-response = requests.get(url)
-print(response)
+response = requests.get(url).text
+soup = BeautifulSoup(response, "lxml")
+sniff = soup.findAll("span", class_="s-item__price")
+print(sniff)
