bc1c824fa7aded9b1acad951a530fd73083f09ff
Roger Lopez <rllopez0410@gmail.com>
2024-03-16 03:53:52 -0400

    downloaded beautiful soup and requests with pip

--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
 
+url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p4432023.m570.l1311&_nkw=gpu+graphics+card&_sacat=0"
+response = requests.get(url)
+print(response)
