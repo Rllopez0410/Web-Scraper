837e0cfc6fa00fb8788831475ed5b64e6df2783e
Roger Lopez <rllopez0410@gmail.com>
2024-03-16 05:06:41 -0400

    created a loop to iterate through each html element and fetch the span child that contained the price text to print each price

--- src/main.py
+++ src/main.py
@@ -4,5 +4,9 @@ import requests
 url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p4432023.m570.l1311&_nkw=gpu+graphics+card&_sacat=0"
 response = requests.get(url).text
 soup = BeautifulSoup(response, "lxml")
-sniff = soup.findAll("span", class_="s-item__price")
-print(sniff)
+allPrices = soup.find_all("div", class_="s-item__detail s-item__detail--primary")
+
+for price in allPrices:
+    priceSpan = price.find("span", class_="s-item__price")
+    if priceSpan:
+        print(priceSpan.text)
