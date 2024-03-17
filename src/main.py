58261978a4d42967675a18142c61f035471098de
Roger Lopez <rllopez0410@gmail.com>
2024-03-17 17:55:54 -0400

    included a replace() to remove any numbers with a comma to become a float intiger in addition to adding a try/except to locate any errors without the programming crashing

--- src/main.py
+++ src/main.py
@@ -9,9 +9,13 @@ allPrices = soup.find_all("div", class_="s-item__detail s-item__detail--primary"
 for price in allPrices:
     priceSpan = price.find("span", class_="s-item__price")
     if priceSpan:
-        priceMod = priceSpan.text[1:]
-        if "to" not in priceMod:
-            castedPrice = float(priceMod)
-            if castedPrice < 20.00:
-                print(f"${castedPrice}")
+        priceText = priceSpan.text[1:].replace(',', '')
+        if "to" not in priceText:
+            try:
+                castedPrice = float(priceText)
+                if castedPrice < 20.00:
+                    print(f"${castedPrice}")
+            except ValueError:
+                print("Unable to convert price to float:", priceText)
+
         
